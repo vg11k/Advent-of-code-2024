1e07788dd733394fcd26483bd5c377f12f99ef5d
vg11k <42996032+vg11k@users.noreply.github.com>
2024-12-05 21:51:24 +0100

    basic template for days

---
+++ template.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <map>
+using namespace std;
+
+//static const string filename = "input.txt";
+static const string filename = "example.txt";
+
+string getCharAt(vector<string> s, int x, int y) {
+    return s.at(x).substr(y, 1);
+}
+
+void loopDoStuff(vector<string> input) {
+    for (string sinput : input) {
+        istringstream iss(sinput);
+        string s;
+        while (getline(iss, s, ' ')) {
+            int val = stoi(s);
+
+        }
+    }
+}
+
+int main(char* args[]) {
+
+    cout << "Hello World !" << endl;
+
+    bool debug = false;
+
+    clock_t start, end;
+
+    ifstream infile(filename.c_str());
+    string line;
+    long long result1 = 0;
+    long long result2 = 0;
+    vector<string> allLines;
+
+    start = clock();
+
+    while (getline(infile, line)) {
+        allLines.push_back(line);
+    }
+    
+
+
+    end = clock();
+
+    cout << "result1 : " << result1 << endl;
+    cout << "result2 : " << result2 << endl;
+    cout << end - start << " ms to compute." << endl;
+
+    cout << "Bye World !" << endl;
+
+    return 0;
+}
